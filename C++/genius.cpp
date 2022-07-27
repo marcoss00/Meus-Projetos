eba4f307c7082f0b884f434fd5250dab77030adf
Marcos Vinicius <marcossvinc@gmail.com>
2022-07-27 20:57:44 -0300

    Add files via upload

---
+++ C++/genius.cpp
@@ -0,0 +1,920 @@
+#include <iostream>
+#include <time.h>
+#include <string>
+#include <stdlib.h>
+#include<windows.h>
+using namespace std;
+
+int cor[12];
+string certo[12];
+
+void pri(){
+	if(cor[0]==0){
+	certo[0]="azul";
+	system("color 10"); //azul
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[0]==1){
+	certo[0]="vermelho";
+	system("color 40"); //vermelho
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[0]==2){
+	certo[0]="amarelo";
+	system("color E0"); //amarelo
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[0]==3){
+	certo[0]="verde";
+	system("color 20"); //verde
+	Sleep(1000);
+	system("color 07");
+}
+}
+void seg(){
+	if(cor[1]==0){
+	certo[1]="azul";
+	system("color 10"); //azul
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[1]==1){
+	certo[1]="vermelho";
+	system("color 40"); //vermelho
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[1]==2){
+	certo[1]="amarelo";
+	system("color E0"); //amarelo
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[1]==3){
+	certo[1]="verde";
+	system("color 20"); //verde
+	Sleep(1000);
+	system("color 07");
+}
+}
+void ter(){
+	if(cor[2]==0){
+	certo[2]="azul";
+	system("color 10"); //azul
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[2]==1){
+	certo[2]="vermelho";
+	system("color 40"); //vermelho
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[2]==2){
+	certo[2]="amarelo";
+	system("color E0"); //amarelo
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[2]==3){
+	certo[2]="verde";
+	system("color 20"); //verde
+	Sleep(1000);
+	system("color 07");
+}
+}
+void qua(){
+	if(cor[3]==0){
+	certo[3]="azul";
+	system("color 10"); //azul
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[3]==1){
+	certo[3]="vermelho";
+	system("color 40"); //vermelho
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[3]==2){
+	certo[3]="amarelo";
+	system("color E0"); //amarelo
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[3]==3){
+	certo[3]="verde";
+	system("color 20"); //verde
+	Sleep(1000);
+	system("color 07");
+}
+}
+void qui(){
+	if(cor[4]==0){
+	certo[4]="azul";
+	system("color 10"); //azul
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[4]==1){
+	certo[4]="vermelho";
+	system("color 40"); //vermelho
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[4]==2){
+	certo[4]="amarelo";
+	system("color E0"); //amarelo
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[4]==3){
+	certo[4]="verde";
+	system("color 20"); //verde
+	Sleep(1000);
+	system("color 07");
+}
+}
+void sex(){
+	if(cor[5]==0){
+	certo[5]="azul";
+	system("color 10"); //azul
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[5]==1){
+	certo[5]="vermelho";
+	system("color 40"); //vermelho
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[5]==2){
+	certo[5]="amarelo";
+	system("color E0"); //amarelo
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[5]==3){
+	certo[5]="verde";
+	system("color 20"); //verde
+	Sleep(1000);
+	system("color 07");
+}
+}
+void set(){
+	if(cor[6]==0){
+	certo[6]="azul";
+	system("color 10"); //azul
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[6]==1){
+	certo[6]="vermelho";
+	system("color 40"); //vermelho
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[6]==2){
+	certo[6]="amarelo";
+	system("color E0"); //amarelo
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[6]==3){
+	certo[6]="verde";
+	system("color 20"); //verde
+	Sleep(1000);
+	system("color 07");
+}
+}
+void oit(){
+	if(cor[7]==0){
+	certo[7]="azul";
+	system("color 10"); //azul
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[7]==1){
+	certo[7]="vermelho";
+	system("color 40"); //vermelho
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[7]==2){
+	certo[7]="amarelo";
+	system("color E0"); //amarelo
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[7]==3){
+	certo[7]="verde";
+	system("color 20"); //verde
+	Sleep(1000);
+	system("color 07");
+}
+}
+void non(){
+	if(cor[8]==0){
+	certo[8]="azul";
+	system("color 10"); //azul
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[8]==1){
+	certo[8]="vermelho";
+	system("color 40"); //vermelho
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[8]==2){
+	certo[8]="amarelo";
+	system("color E0"); //amarelo
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[8]==3){
+	certo[8]="verde";
+	system("color 20"); //verde
+	Sleep(1000);
+	system("color 07");
+}
+}
+void dec(){
+	if(cor[9]==0){
+	certo[9]="azul";
+	system("color 10"); //azul
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[9]==1){
+	certo[9]="vermelho";
+	system("color 40"); //vermelho
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[9]==2){
+	certo[9]="amarelo";
+	system("color E0"); //amarelo
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[9]==3){
+	certo[9]="verde";
+	system("color 20"); //verde
+	Sleep(1000);
+	system("color 07");
+}
+}
+void de1(){
+	if(cor[10]==0){
+	certo[10]="azul";
+	system("color 10"); //azul
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[10]==1){
+	certo[10]="vermelho";
+	system("color 40"); //vermelho
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[10]==2){
+	certo[10]="amarelo";
+	system("color E0"); //amarelo
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[10]==3){
+	certo[10]="verde";
+	system("color 20"); //verde
+	Sleep(1000);
+	system("color 07");
+}
+}
+void de2(){
+	if(cor[11]==0){
+	certo[11]="azul";
+	system("color 10"); //azul
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[11]==1){
+	certo[11]="vermelho";
+	system("color 40"); //vermelho
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[11]==2){
+	certo[11]="amarelo";
+	system("color E0"); //amarelo
+	Sleep(1000);
+	system("color 07");
+}
+
+if(cor[11]==3){
+	certo[11]="verde";
+	system("color 20"); //verde
+	Sleep(1000);
+	system("color 07");
+}
+}
+
+int main(int argc, char** argv) {
+string coruser[12];
+srand(time(0));
+
+for(int k=0; k<12; k++){
+	Sleep(rand()%70);
+cor[k]=rand()%4;
+}
+cout<<"\t\tG E N I U S\n\n";
+
+system("pause");
+system("CLS");
+
+pri();
+Sleep(500);
+
+cin>>coruser[0];
+if(coruser[0]!=certo[0]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+
+system("CLS");
+
+pri();
+Sleep(500);
+seg();
+
+cin>>coruser[0]>>coruser[1];
+
+if(coruser[0]!=certo[0]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[1]!=certo[1]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+
+system("CLS");
+
+Sleep(500);
+pri();
+Sleep(500);
+seg();
+Sleep(500);
+ter();
+
+cin>>coruser[0]>>coruser[1]>>coruser[2];
+
+if(coruser[0]!=certo[0]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[1]!=certo[1]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[2]!=certo[2]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+
+system("CLS");
+
+Sleep(500);
+pri();
+Sleep(500);
+seg();
+Sleep(500);
+ter();
+Sleep(500);
+qua();
+
+cin>>coruser[0]>>coruser[1]>>coruser[2]>>coruser[3];
+
+if(coruser[0]!=certo[0]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[1]!=certo[1]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[2]!=certo[2]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[3]!=certo[3]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+
+system("CLS");
+
+Sleep(500);
+pri();
+Sleep(500);
+seg();
+Sleep(500);
+ter();
+Sleep(500);
+qua();
+Sleep(500);
+qui();
+
+cin>>coruser[0]>>coruser[1]>>coruser[2]>>coruser[3]>>coruser[4];
+
+if(coruser[0]!=certo[0]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[1]!=certo[1]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[2]!=certo[2]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[3]!=certo[3]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[4]!=certo[4]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+
+system("CLS");
+
+Sleep(500);
+pri();
+Sleep(500);
+seg();
+Sleep(500);
+ter();
+Sleep(500);
+qua();
+Sleep(500);
+qui();
+Sleep(500);
+sex();
+
+cin>>coruser[0]>>coruser[1]>>coruser[2]>>coruser[3]>>coruser[4]>>coruser[5];
+
+if(coruser[0]!=certo[0]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[1]!=certo[1]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[2]!=certo[2]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[3]!=certo[3]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[4]!=certo[4]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[5]!=certo[5]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+
+system("CLS");
+
+Sleep(500);
+pri();
+Sleep(500);
+seg();
+Sleep(500);
+ter();
+Sleep(500);
+qua();
+Sleep(500);
+qui();
+Sleep(500);
+sex();
+Sleep(500);
+set();
+
+cin>>coruser[0]>>coruser[1]>>coruser[2]>>coruser[3]>>coruser[4]>>coruser[5]>>coruser[6];
+
+if(coruser[0]!=certo[0]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[1]!=certo[1]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[2]!=certo[2]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[3]!=certo[3]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[4]!=certo[4]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[5]!=certo[5]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[6]!=certo[6]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+
+system("CLS");
+
+Sleep(500);
+pri();
+Sleep(500);
+seg();
+Sleep(500);
+ter();
+Sleep(500);
+qua();
+Sleep(500);
+qui();
+Sleep(500);
+sex();
+Sleep(500);
+set();
+Sleep(500);
+oit();
+
+cin>>coruser[0]>>coruser[1]>>coruser[2]>>coruser[3]>>coruser[4]>>coruser[5]>>coruser[6]>>coruser[7];
+
+if(coruser[0]!=certo[0]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[1]!=certo[1]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[2]!=certo[2]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[3]!=certo[3]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[4]!=certo[4]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[5]!=certo[5]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[6]!=certo[6]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[7]!=certo[7]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+
+system("CLS");
+
+Sleep(500);
+pri();
+Sleep(500);
+seg();
+Sleep(500);
+ter();
+Sleep(500);
+qua();
+Sleep(500);
+qui();
+Sleep(500);
+sex();
+Sleep(500);
+set();
+Sleep(500);
+oit();
+Sleep(500);
+non();
+
+cin>>coruser[0]>>coruser[1]>>coruser[2]>>coruser[3]>>coruser[4]>>coruser[5]>>coruser[6]>>coruser[7]>>coruser[8];
+
+if(coruser[0]!=certo[0]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[1]!=certo[1]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[2]!=certo[2]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[3]!=certo[3]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[4]!=certo[4]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[5]!=certo[5]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[6]!=certo[6]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[7]!=certo[7]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[8]!=certo[8]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+
+system("CLS");
+
+Sleep(500);
+pri();
+Sleep(500);
+seg();
+Sleep(500);
+ter();
+Sleep(500);
+qua();
+Sleep(500);
+qui();
+Sleep(500);
+sex();
+Sleep(500);
+set();
+Sleep(500);
+oit();
+Sleep(500);
+non();
+Sleep(500);
+dec();
+
+cin>>coruser[0]>>coruser[1]>>coruser[2]>>coruser[3]>>coruser[4]>>coruser[5]>>coruser[6]>>coruser[7]>>coruser[8]>>coruser[9];
+
+if(coruser[0]!=certo[0]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[1]!=certo[1]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[2]!=certo[2]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[3]!=certo[3]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[4]!=certo[4]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[5]!=certo[5]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[6]!=certo[6]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[7]!=certo[7]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[8]!=certo[8]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[9]!=certo[9]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+
+system("CLS");
+
+Sleep(500);
+pri();
+Sleep(500);
+seg();
+Sleep(500);
+ter();
+Sleep(500);
+qua();
+Sleep(500);
+qui();
+Sleep(500);
+sex();
+Sleep(500);
+set();
+Sleep(500);
+oit();
+Sleep(500);
+non();
+Sleep(500);
+dec();
+Sleep(500);
+de1();
+
+cin>>coruser[0]>>coruser[1]>>coruser[2]>>coruser[3]>>coruser[4]>>coruser[5]>>coruser[6]>>coruser[7]>>coruser[8]>>coruser[9]>>coruser[10];
+
+if(coruser[0]!=certo[0]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[1]!=certo[1]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[2]!=certo[2]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[3]!=certo[3]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[4]!=certo[4]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[5]!=certo[5]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[6]!=certo[6]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[7]!=certo[7]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[8]!=certo[8]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[9]!=certo[9]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[10]!=certo[10]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+
+system("CLS");
+
+Sleep(500);
+pri();
+Sleep(500);
+seg();
+Sleep(500);
+ter();
+Sleep(500);
+qua();
+Sleep(500);
+qui();
+Sleep(500);
+sex();
+Sleep(500);
+set();
+Sleep(500);
+oit();
+Sleep(500);
+non();
+Sleep(500);
+dec();
+Sleep(500);
+de1();
+Sleep(500);
+de2();
+
+cin>>coruser[0]>>coruser[1]>>coruser[2]>>coruser[3]>>coruser[4]>>coruser[5]>>coruser[6]>>coruser[7]>>coruser[8]>>coruser[9]>>coruser[10]>>coruser[11];
+
+if(coruser[0]!=certo[0]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[1]!=certo[1]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[2]!=certo[2]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[3]!=certo[3]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[4]!=certo[4]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[5]!=certo[5]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[6]!=certo[6]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[7]!=certo[7]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[8]!=certo[8]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[9]!=certo[9]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[10]!=certo[10]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+if(coruser[11]!=certo[11]){
+		cout<<"Voce Errou!!";
+		goto final;	
+}
+system("CLS");
+cout<<"\nVOCE GANHOU!!!!!!!!!";
+for(int l=0; l<0; l++){
+	system("color 10");
+	system("color 40");
+	system("color 20");
+	system("color E0");
+}
+
+final:
+	return 0;
+}
+
